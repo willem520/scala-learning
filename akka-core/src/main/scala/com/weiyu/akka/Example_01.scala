e2c73cabf2da290ace704e5301aa89a9a950d573
weiyu <weiyu@gomeholdings.com>
2018-03-16 17:32:25 +0800

    调整结构

---
+++ akka-core/src/main/scala/com/weiyu/akka/Example_01.scala
@@ -0,0 +1,31 @@
+package com.weiyu.bigData.akka
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+
+object Example_01 extends App {
+
+  class MyActor extends Actor {
+    val log = Logging(context.system,this)
+
+    override def receive : Receive = {
+      case "test" => log.info("received test")
+      case _ => log.info("received unknown message")
+    }
+  }
+
+  //创建ActorSystem对象
+  val system = ActorSystem("MyActorSystem")
+  //返回ActorSystem的LoggingAdapter
+  val systemLog = system.log
+  //创建MyActor，指定actor名称为myactor
+  val myactor = system.actorOf(Props[MyActor],name = "myactor")
+
+  systemLog.info("准备向myactor发送消息")
+  //向myactor发送消息
+  myactor!"test"
+  myactor!123
+
+  //关闭ActorSystem，停止程序的运行
+  system.terminate()
+}
