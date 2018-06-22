a5415c237faf242752f9939bff1d4c497f3147ab
weiyu <weiyu@gomeholdings.com>
2018-06-22 15:44:09 +0800

    添加基本类型简单示例代码

---
+++ src/main/scala/willem/weiyu/scala/basicType/BasicTypeDemo.scala
@@ -0,0 +1,13 @@
+package willem.weiyu.scala.basicType
+
+object BasicTypeDemo {
+
+  def main(args: Array[String]): Unit = {
+    println("hello" * 3)
+    println("============")
+    println(10 max 2)
+    println("============")
+    println(BigInt(2).pow(3))
+    println("============")
+  }
+}
