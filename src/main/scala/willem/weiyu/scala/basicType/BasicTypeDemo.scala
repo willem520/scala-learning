b64aff77c02574a4c2906bdb6274f10d3bd84b26
weiyu <weiyu@gomeholdings.com>
2018-07-30 16:09:14 +0800

    添加注释

--- src/main/scala/willem/weiyu/scala/basicType/BasicTypeDemo.scala
+++ src/main/scala/willem/weiyu/scala/basicType/BasicTypeDemo.scala
@@ -9,5 +9,21 @@ object BasicTypeDemo {
     println("============")
     println(BigInt(2).pow(3))
     println("============")
+    val str =
+      """
+        hello,
+        multi line
+        bye
+      """
+    println(str)
+    println("============")
+
+    //var定义变量后，可以更改
+    var str1 = "hello"
+    str1 = "hey"
+
+    //val定义变量后，无法更改
+    val str2 = "hello"
+    //str2 = "hey"
   }
 }
