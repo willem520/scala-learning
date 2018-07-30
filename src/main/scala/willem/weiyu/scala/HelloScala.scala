c3126c014131ad008ef523f37f0b64c5df8c84d0
weiyu <weiyu@gomeholdings.com>
2018-07-30 15:48:48 +0800

    添加注释

--- src/main/scala/willem/weiyu/scala/HelloScala.scala
+++ src/main/scala/willem/weiyu/scala/HelloScala.scala
@@ -1,12 +1,18 @@
 package willem.weiyu.scala
 
+// 重命名成员
+import java.util.{HashMap => JHashMap}
+// 引入java.util包的所有成员，但隐藏ArrayList
+import java.util.{ArrayList => _,_}
 /**
   * @author weiyu
   * @create 2017/9/16
+  * 默认情况下，Scala总会引入java.lang._ 、scala._和Predef._
   */
 object HelloScala {
 
   def main(args: Array[String]): Unit = {
-    println("======hello Scala!")
+    val map :JHashMap[String, String] = new JHashMap[String,String]()
+    println("======Hello Scala!")
   }
 }
