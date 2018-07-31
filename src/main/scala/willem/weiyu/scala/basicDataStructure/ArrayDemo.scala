0976d3db802514832336d4e2701277565c18e0be
weiyu <weiyu@gomeholdings.com>
2018-07-31 14:38:07 +0800

    添加二维数组示例

--- src/main/scala/willem/weiyu/scala/basicDataStructure/ArrayDemo.scala
+++ src/main/scala/willem/weiyu/scala/basicDataStructure/ArrayDemo.scala
@@ -10,12 +10,23 @@ object ArrayDemo {
   def main(args: Array[String]): Unit = {
     val numberArray = new Array[Int](10)
     numberArray.foreach(println)
+    println("======")
 
-    println("******")
     //这种调用方式其实是调用其apply方法进行数组创建操作
     val strArray = Array("first","second")
     strArray(1) = "hello"
     strArray.foreach(println)
+    println("======")
+
+    val myArr = Array.ofDim[Int](3,3)
+
+    for(i <- 0 to 2; j <- 0 to 2){
+      myArr(i)(j) = j
+    }
+
+    for(i <- 0 to 2; j <- 0 to 2){
+      println("======i,j="+myArr(i)(j))
+    }
   }
 
 }
